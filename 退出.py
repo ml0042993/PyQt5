1c6fee11252f6d3837c018c9bcd518724c90c906
ml0042993 <zhoujianxiong.1988@gmail.com>
2020-03-19 18:12:42 +0800

    添加注释

--- 退出.py
+++ 退出.py
@@ -16,6 +16,7 @@ class QuitApp(QMainWindow):
 		#信号与槽绑定
 		self.button1.clicked.connect(self.onClick_Button)
 		#创建水平布局layout
+
 		layout = QHBoxLayout()
 		#水平布局上加入按钮button1
 		layout.addWidget(self.button1)
