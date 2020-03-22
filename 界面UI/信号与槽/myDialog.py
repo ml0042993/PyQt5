6191633b793dda27f5389c84ac0fd1375cc63eeb
ml0042993 <zhoujianxiong.1988@gmail.com>
2020-03-22 20:38:56 +0800

    添加导入包QtCore，添加斜体功能，使用@pyqtSolt修饰符声明参数类型

--- 界面UI/信号与槽/myDialog.py
+++ 界面UI/信号与槽/myDialog.py
@@ -3,6 +3,9 @@
 
 import sys
 from PyQt5.QtWidgets import QApplication,QDialog
+from PyQt5 import QtCore#包含pyqtSolt
+
+# from PyQt5.QtCore import pyqtSolt
 from ui_Dialog import Ui_Dialog
 
 class QmyDialog(QDialog):
@@ -17,7 +20,7 @@ class QmyDialog(QDialog):
 
     def on_checkBoxBold_toggled(self,checked):
         #加粗，checked是勾选状态是一个bool型
-        # print(checked)
+        print(checked)
         font =self.ui.TextEdit.font()
         # print(font)
         font.setBold(checked)
@@ -29,7 +32,16 @@ class QmyDialog(QDialog):
         font = self.ui.TextEdit.font()
         font.setUnderline(checked)
         self.ui.TextEdit.setFont(font)
-
+    @QtCore.pyqtSlot(bool)#书上未说明要导入QtCore
+    #该修饰符的作用是将函数的参数类型声明清楚，让函数明确使用的是哪一种信号（clicked（）或者是clicked(bool），这里用于区分overload型信号的选择
+    def on_checkBoxItalic_clicked(self,checked):#添加新功能
+        print(checked)
+        font = self.ui.TextEdit.font()
+        print(font)
+        font.setItalic(checked)
+        print(12)
+        self.ui.TextEdit.setFont(font)
+        print(13)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
