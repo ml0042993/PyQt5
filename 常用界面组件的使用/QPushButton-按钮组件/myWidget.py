506a51ba0cf1ff13d3f8ddd3b33aada9f6d44fa0
ml0042993 <zhoujianxiong.1988@gmail.com>
2020-03-24 22:16:57 +0800

    1.修改ui设计中的命名及组件选择，之前存在将文本框选择错误的问题，导致部分选项无法使用，原因为，组件无槽函数中设置的方法 2.添加注释，说明组件作用及功能

--- 常用界面组件的使用/QPushButton-按钮组件/myWidget.py
+++ 常用界面组件的使用/QPushButton-按钮组件/myWidget.py
@@ -1,7 +1,7 @@
 import sys
 from PyQt5.QtWidgets import QApplication,QWidget
 from PyQt5.QtCore import pyqtSlot,Qt
-from PyQt5.QtGui import QFont
+# from PyQt5.QtGui import QFont
 # from PyQt5.QtWidgets import
 # from PyQt5.QtSql import
 # from PyQt5.QtMultimedia import
@@ -19,28 +19,43 @@ class QmyWidget(QWidget):
 	##==========事件处理函数===========
 
 	##==========由connectSlotsByName()自动关联的槽函数====
-	def on_btnAlign_Left_clicked(self):
+
+	'''
+	editInput：为QLineEdit组件名称
+	setAlignment()：设置对齐方式
+	'''
+	def on_btnAlign_Left_clicked(self):#居左
 		self.ui.editInput.setAlignment(Qt.AlignLeft)
-	def on_btnAlign_Center_clicked(self):
+	def on_btnAlign_Center_clicked(self):#居中
 		self.ui.editInput.setAlignment(Qt.AlignCenter)
-	def on_btnAlign_Right_clicked(self):
+	def on_btnAlign_Right_clicked(self):#居右
 		self.ui.editInput.setAlignment(Qt.AlignRight)
 
 	@pyqtSlot(bool)
-	def on_btnFont_Bold_clicked(self,checked):
-		font = self.ui.editInput.font()
-		font.setBold(checked)
-		self.ui.editInput.setFont(font)
+	def on_btnFont_Bold_clicked(self,checked):#粗体
+		font = self.ui.editInput.font()#获取字体对象
+		font.setBold(checked)#checked=True，即当参数为真时，setBold()将font设置为粗体
+		self.ui.editInput.setFont(font)#设置字体
 	@pyqtSlot(bool)
-	def on_btnFont_Italic_clicked(self,checked):
+	def on_btnFont_Italic_clicked(self,checked):#斜体
 		font = self.ui.editInput.font()
 		font.setItalic(checked)
 		self.ui.editInput.setFont(font)
 	@pyqtSlot(bool)
-	def on_btnFont_UnderLine_clicked(self,checked):
+	def on_btnFont_UnderLine_clicked(self,checked):#下划线
 		font = self.ui.editInput.font()
 		font.setUnderline(checked)
 		self.ui.editInput.setFont(font)
+
+	@pyqtSlot(bool)
+	def on_chkBox_Readonly_clicked(self,checked):#只读
+		self.ui.editInput.setReadOnly(checked)
+	@pyqtSlot(bool)
+	def on_chkBox_Enable_clicked(self,checked):#锁定QLineEdit组件
+		self.ui.editInput.setEnabled(checked)
+	@pyqtSlot(bool)
+	def on_chkBox_ClearButton_clicked(self,checked):#聚焦后清除原有内容
+		self.ui.editInput.setClearButtonEnabled(checked)
 	##=========自定义槽函数============
 
 	##===========窗体测试程序==========
