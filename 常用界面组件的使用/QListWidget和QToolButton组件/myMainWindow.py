3c25b3bc3de70c16b7d6fcbdf6d506a5b0ee0d22
ml0042993 <zhoujianxiong.1988@gmail.com>
2020-03-28 23:47:25 +0800

    1.设计ui中的组件，设定Action和ui中的信号与槽函数 2.设定工具栏toolButton的下拉菜单的各项参数 3.设置左侧toolButton与action关联

--- 常用界面组件的使用/QListWidget和QToolButton组件/myMainWindow.py
+++ 常用界面组件的使用/QListWidget和QToolButton组件/myMainWindow.py
@@ -1,5 +1,5 @@
 import sys
-from PyQt5.QtWidgets import QApplication,QMainWindow
+from PyQt5.QtWidgets import QApplication,QMainWindow,QToolButton,QMenu
 from PyQt5.QtCore import pyqtSlot,pyqtSignal,Qt
 # from PyQt5.QtGui import
 # from PyQt5.QtWidgets import
@@ -14,7 +14,52 @@ class QmyMainWindow(QMainWindow):
 		super().__init__(parent)#调用父类构造函数,创建窗体
 		self.ui = Ui_MainWindow()#创建Ui对象
 		self.ui.setupUi(self)#构造UI
+
+		self.__setActionsForButton()
+		self.__createSelectionPopMenu()
 	##==========自定义功能函数==========
+	def __setActionsForButton(self):
+		'''
+		QToolButton存在一个setDefaultAction（）函数，可以使其与一个Action相关联，按钮的文字、图标、tooltip都与关联的Action一致；
+		单击QToolButton会执行action的槽函数；
+		主工具栏上的按钮是根据Action自动创建的QToolButton
+		:return:
+		'''
+		self.ui.btnList_Ini.setDefaultAction(self.ui.actList_Ini)
+		self.ui.btnList_Clear.setDefaultAction(self.ui.actList_Clear)
+		self.ui.btnList_Insert.setDefaultAction(self.ui.actList_Insert)
+		self.ui.btnList_Append.setDefaultAction(self.ui.actList_Append)
+		self.ui.btnList_Delete.setDefaultAction(self.ui.actList_Delete)
+
+		self.ui.btnSel_ALL.setDefaultAction(self.ui.actSel_All)
+		self.ui.btnSel_None.setDefaultAction(self.ui.actSel_None)
+		self.ui.btnSel_Invs.setDefaultAction(self.ui.actSel_Invs)
+
+	def __createSelectionPopMenu(self):
+		'''
+		setMenu()可以为其设置一个下拉菜单，配合QToolButton的一些属性，可以有不同的下拉菜单效果
+		:return:
+		'''
+		menuSelection = QMenu(self)#初始化
+		#加入三个功能
+		menuSelection.addAction(self.ui.actSel_All)
+		menuSelection.addAction(self.ui.actSel_None)
+		menuSelection.addAction(self.ui.actSel_Invs)
+
+
+		#工具栏上的下拉菜单按钮
+		self.ui.mainToolBar.addSeparator()#分割线
+		self.__btnSelectItems_2 = QToolButton(self)#初始化一个QToolButton按钮
+		# self.__btnSelectItems_2.setPopupMode(QToolButton.InstantPopup)#设置下拉菜单的弹出模式
+		self.__btnSelectItems_2.setPopupMode(QToolButton.MenuButtonPopup)#设置下拉菜单的弹出模式
+		self.__btnSelectItems_2.setDefaultAction(self.ui.actSelPopMenu)#利用setDefaultAction()函数关联Action
+		self.__btnSelectItems_2.setToolButtonStyle(Qt.ToolButtonTextUnderIcon)#设定图标显示格式
+		self.__btnSelectItems_2.setMenu(menuSelection)#为按钮__btnSelectItems_2 设置下拉菜单
+		# self.__btnSelectItems_2.setMinimumSize(50,10)
+
+		self.ui.mainToolBar.addWidget(self.__btnSelectItems_2)
+		self.ui.mainToolBar.addAction(self.ui.actQuit)
+
 
 	##==========事件处理函数===========
 
