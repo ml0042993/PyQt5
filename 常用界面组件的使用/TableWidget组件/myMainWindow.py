cb4664bf6e6d3b0deeb3eb6f9ff7eb779000b18d
ml0042993 <zhoujianxiong.1988@gmail.com>
2020-04-02 10:24:06 +0800

    定义两个枚举类,CellType表示单元格类型,FieldColNum表示每个列的编号,初始化状态栏

--- 常用界面组件的使用/TableWidget组件/myMainWindow.py
+++ 常用界面组件的使用/TableWidget组件/myMainWindow.py
@@ -1,6 +1,7 @@
 import sys
-from PyQt5.QtWidgets import QApplication,QMainWindow
+from PyQt5.QtWidgets import QApplication,QMainWindow,QLabel
 from PyQt5.QtCore import pyqtSlot,pyqtSignal,Qt
+from enum import Enum
 # from PyQt5.QtGui import
 # from PyQt5.QtWidgets import
 # from PyQt5.QtSql import
@@ -9,11 +10,41 @@ from PyQt5.QtCore import pyqtSlot,pyqtSignal,Qt
 
 from ui_MainWindow import Ui_MainWindow
 
+class CellType(Enum):
+	ctName = 1000
+	ctSex = 1001
+	ctBirth = 1002
+	cnNation = 1003
+	cnScore = 1004
+	ctPartyM = 1005
+
+class FieldColNum(Enum):
+	colName = 0
+	colSex = 1
+	colBirth = 2
+	colNation = 3
+	colScore = 4
+	colParthM = 5
+
 class QmyMainWindow(QMainWindow):
 	def __init__(self,parent=None):
 		super().__init__(parent)#调用父类构造函数,创建窗体
 		self.ui = Ui_MainWindow()#创建Ui对象
 		self.ui.setupUi(self)#构造UI
+
+		self.LabCellIndex = QLabel('当前单元格坐标: ',self)
+		self.LabCellIndex.setMinimumWidth(250)
+		self.LabCellType = QLabel('当前单元格类型: ',self)
+		self.LabCellType.setMinimumWidth(200)
+		self.LabStudID = QLabel('学生ID: ',self)
+		self.LabStudID.setMinimumWidth(200)
+
+		self.ui.statusBar.addWidget(self.LabCellIndex)
+		self.ui.statusBar.addWidget(self.LabCellType)
+		self.ui.statusBar.addWidget(self.LabStudID)
+
+		self.ui.tableInfo.setAlternatingRowColors(True)#交替行颜色
+
 	##==========自定义功能函数==========
 
 	##==========事件处理函数===========
