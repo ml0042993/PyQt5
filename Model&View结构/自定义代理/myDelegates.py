3f53045b4f2914f82fd5ff90e9933ccf3d3b3dff
ml0042993 <zhoujianxiong.1988@gmail.com>
2020-04-10 17:14:12 +0800

    添加QComboBox的自定义代理

--- Model&View结构/自定义代理/myDelegates.py
+++ Model&View结构/自定义代理/myDelegates.py
@@ -55,3 +55,33 @@ class QmyFloatSpinDelegate(QStyledItemDelegate):
 		:return:
 		'''
 		editor.setGeometry(option.rect)
+
+class QmComboBoxDelegate(QStyledItemDelegate):
+	def __init__(self,parent=None):
+		super().__init__(parent)
+		self.__itemList = []
+		self.__isEditable = False
+
+	def setItems(self,itemList, isEditable = False):
+		self.__itemList = itemList
+		self.__isEditable = isEditable
+
+	def createEditor(self, parent, option, index):
+		editor = QComboBox(parent)
+		editor.setFrame(False)
+		editor.setEditable(self.__isEditable)
+		editor.addItems(self.__itemList)
+		return editor
+
+	def setEditorData(self, editor, index):
+		model = index.model()
+		text = model.data(index,Qt.EditRole)
+		editor.setCurrentText(text)
+
+	def setModelData(self, editor, model, index):
+		text = editor.currentText()
+		model.setData(index,text,Qt.EditRole)
+
+	def updateEditorGeometry(self, editor, option, index):
+		editor.setGeometry(option.rect)
+
